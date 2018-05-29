82f0fff511dd11aafe9006d839fa4162a791dce2
Deepak Kumar <deepak.113016@gmail.com>
2018-05-29 15:02:54 +0530

    issue fixes

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ var exp = require("express");
 var app = exp();
 
 var port = process.env.PORT || 4000;
+app.get('/',function(req,res){
+    res.send("Hello World");
+})
 app.get('/players', function (req, res) {
     var query = req.query;
     console.log(" query value = " + JSON.stringify(query));
     res.write("name= "+ query.name);
     res.end("  lang= "+ query.lang);
 })
+
 
 app.listen(port, function () {
     console.log("server is listening at port " + port);
